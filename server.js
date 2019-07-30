3bc281705354942ffc411ab45205c9a0a1c24425
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-07-30 21:07:29 +0100

    add cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const authRouter = require('./auth/authRouter');
 const server = express();
 
 server.use(helmet());
+server.use(cors());
 server.use(express.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use('/api/auth', authRouter);
